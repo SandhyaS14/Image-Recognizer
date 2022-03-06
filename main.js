65c4a228499003d3d90394f54a95fcb43f941d96
SandhyaS14 <85371666+SandhyaS14@users.noreply.github.com>
2022-03-06 14:48:44 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 Webcam.set({
-    width: 530,
-    height: 500,
+    width: 350,
+    height: 300,
     image_format: "png",
     png_quality: 90
 }); 
-Webcam.attach("#camera");
+camera = document.getElementById("camera");
+Webcam.attach(camera);
 function takesnap(){
     Webcam.snap(function (data_uri){
         document.getElementById("output").innerHTML = "<img id='captured_image' src =" + data_uri + ">";
@@ -12,7 +13,7 @@ function takesnap(){
 }
 
 console.log("ml5.version", ml5.version);
-classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/1gcYv3uST/", model_loaded);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/1gcYv3uST/model.json", model_loaded);
 
 function model_loaded() {
     console.log("model loaded.");
