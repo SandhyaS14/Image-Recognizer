77c5ca8f8d9560e42f48e724c818c9d37acd1bf8
SandhyaS14 <85371666+SandhyaS14@users.noreply.github.com>
2022-03-12 13:24:58 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -13,8 +13,24 @@ function takesnap(){
 }
 
 console.log("ml5.version", ml5.version);
-classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/1gcYv3uST/model.json", model_loaded);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/eRjVQEkd8/model.json", model_loaded);
 
 function model_loaded() {
     console.log("model loaded.");
+}
+
+function check(){
+    img = document.getElementById("captured_image");
+    classifier.classify(img, getResult);
+}
+
+function getResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else {
+        console.log(results);
+        document.getElementById("objectname").innerHTML = results[0].label;
+        document.getElementById("objectaccuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
 }
